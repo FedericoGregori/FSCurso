a25369c55265effe3cc379037f27236334637660
Damián Paolo Rotta <dipiert@gmail.com>
2018-10-09 13:45:11 -0300

    [FIX] field.length <- field.value.length

--- Unidad 1 - Javascript y el DOM/Evaluación 1/javascript.js
+++ Unidad 1 - Javascript y el DOM/Evaluación 1/javascript.js
@@ -10,51 +10,51 @@ const {
 const error = document.getElementById('error');
 const MINIMUN_LENGTH = 8;
 
-function validarFormulario(e){
-    error.innerHTML = '';
-    if (!isValidForm()) {
-        e.preventDefault();
-    }
+function validarFormulario(e) {
+  error.innerHTML = '';
+  if (!isValidForm()) {
+    e.preventDefault();
+  }
 }
 
 function isValidForm() {
-    return isNotEmpty(nombre, "el nombre")
-        & isNotEmpty(apellido, "el apellido")
-        & isNotEmpty(email, "el correo electronico")
-        & isNotEmpty(nac, "la fecha de nacimiento")
-        & isValidLength(pass, "La contraseña")
-        & isValidLength(user, "El nombre de usuario")
+  return isNotEmpty(nombre, "el nombre")
+      & isNotEmpty(apellido, "el apellido")
+      & isNotEmpty(email, "el correo electronico")
+      & isNotEmpty(nac, "la fecha de nacimiento")
+      & isValidLength(pass, "La contraseña")
+      & isValidLength(user, "El nombre de usuario")
 }
 
 function isNotEmpty(field, field_name) {
-    if (field.value === '' || field.value === undefined) {
-        error.style.display = 'block';
-        error.innerHTML += '<li>Por favor completa ' + field_name + '</li>';
-        return false;
-    }
-    return true;
+  if (field.value === '' || field.value === undefined) {
+    error.style.display = 'block';
+    error.innerHTML += '<li>Por favor completa ' + field_name + '</li>';
+    return false;
+  }
+  return true;
 }
 
 function isValidLength(field, field_name) {
-    if (field.length === undefined || field.length < MINIMUN_LENGTH) {
-        error.style.display = 'block';
-        error.innerHTML += '<li> ' + field_name  + ' debe tener al menos ' + MINIMUN_LENGTH + ' caracteres' + '</li>';
-        return false;
-    }
-   return true;
+  if (field.value.length === undefined || field.value.length < MINIMUN_LENGTH) {
+    error.style.display = 'block';
+    error.innerHTML += '<li> ' + field_name + ' debe tener al menos ' + MINIMUN_LENGTH + ' caracteres' + '</li>';
+    return false;
+  }
+  return true;
 }
 
 formulario.addEventListener('submit', validarFormulario);
 let today = new Date();
 let dd = today.getDate();
-let mm = today.getMonth()+1;
+let mm = today.getMonth() + 1;
 const yyyy = today.getFullYear();
- if(dd<10){
-        dd='0'+dd
-    }
-    if(mm<10){
-        mm='0'+mm
-    }
+if (dd < 10) {
+  dd = '0' + dd
+}
+if (mm<10) {
+  mm = '0' + mm
+}
 
-today = yyyy+'-'+mm+'-'+dd;
+today = yyyy + '-' + mm + '-' + dd;
 document.getElementById("nac").setAttribute("max", today);
